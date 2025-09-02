e6e069a17abb163823bc3a8ac1bd8491b79f8adc
Naveen-pntk32 <pnavaneethakrishnan07@gmail.com>
2025-09-02 12:08:38 +0530

    fix:vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     port: 3000
+  },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
